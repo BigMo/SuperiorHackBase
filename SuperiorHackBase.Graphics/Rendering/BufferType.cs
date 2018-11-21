942b4dc1f8883e4da2a0104891f9b4ead655b6a3
D34Dspy <d.reckruehm@gmail.com>
2018-11-21 20:30:32 +0100

    Created Renderer Design

---
+++ SuperiorHackBase.Graphics/Rendering/BufferType.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SuperiorHackBase.Graphics.Rendering
+{
+    public enum BufferType
+    {
+        VertexBuffer,
+        IndexBuffer,
+        ShaderInfo,
+        Texture,
+        //DepthBuffer,
+        VertexShader,
+        PixelShader,
+    }
+}
---
+++ SuperiorHackBase.Graphics/Rendering/IGraphicsDevice.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SuperiorHackBase.Graphics.Rendering
+{
+    public interface IGraphicsDevice : IDisposable
+    {
+        void CreateVertexBuffer(AttributeHost attributeHost, int maxVertices);
+        void CreateVertexBuffer(int size);
+
+        void CreateIndexBuffer(Format format, int maxIndices);
+        void CreateIndexBuffer(int size);
+
+        void CreateTexture(Format format, int width, int height);
+
+        void CreateDescriptorView(IGraphicsBuffer graphicsBuffer, DescriptorViewType type);
+
+        ICommandList CreateCommandList();
+
+        void ExecuteCommandList(ICommandList commandList);
+    }
+}
